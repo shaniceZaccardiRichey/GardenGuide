21e96402c9bf3935abb52bdde5690cdb95364b85
Shanice A Zaccardi-Richey <shanice.zaccardi.richey@gmail.com>
2021-05-16 19:15:02 -0500

    Corrected plantsArray

--- scripts/companion-planting.js
+++ scripts/companion-planting.js
@@ -4,8 +4,8 @@ var plantsArray = [];
 const loadPlants = async () => {
     try {
         const res = await fetch('https://shanicezaccardirichey.github.io/GardenGuide/json/companion-plants.json');
-        plantsList = await res.json();
-        displayPlants(plantsList);
+        plantsArray = await res.json();
+        displayPlants(plantsArray);
     } catch (err) {
         console.error(err);
     }
