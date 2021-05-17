8e9f2f7395429cfa1741cc3af2b90eefe3186def
Shanice A Zaccardi-Richey <shanice.zaccardi.richey@gmail.com>
2021-05-16 19:31:16 -0500

    Add listed companion plants and notes

--- scripts/companion-planting.js
+++ scripts/companion-planting.js
@@ -14,14 +14,20 @@ const loadPlants = async () => {
 const displayPlants = (plants) => {
     const htmlString = plants
         .map((plant) => {
+            const companions = plant.companions.map(companion => `<li>${companion}`);
+            const notes = plant.notes.map(note => `<li>${note}`);
+
             return `
             <li class="plant">
                 <h2>${plant.name}</h2>
+                <h3>Companion Plants</h3>
+                <ul>${companions.join('')}</ul>
+                <h3>Notes</h3>
+                <ul>${notes.join('')}</ul>
             </li>
-		`;
+        `;
         })
         .join('');
     plantsList.innerHTML = htmlString;
 };
-
 loadPlants();
